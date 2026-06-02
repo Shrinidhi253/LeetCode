26971e8bca151c85e2f50a9e057c7a95e99d098c
Shrinidhi253 <shrinidhi.ganapathi@gmail.com>
2026-06-02 21:36:45 +0200

    #20 Add reverse num solution for palindrome number problem

--- src/easy/PalindromeNumber_9.java
+++ src/easy/PalindromeNumber_9.java
@@ -13,7 +13,8 @@ public class PalindromeNumber_9 {
     But it is very similar to converting an integer into a string which we were asked not
     to do in the problem .... T-T
     */
-    public boolean isPalindrome(int x) {
+
+    public boolean isPalindromeSolution1(int x) {
         if (x < 0) {return false;}
         if (x < 10) {return true;}
 
@@ -33,5 +34,39 @@ public class PalindromeNumber_9 {
             right--;
         }
         return true;
+    }
+
+    /*
+    Idea:
+    The array in solution 1 actually adds the digits in the reverse order because of the %
+    e.g. 123 -> [3, 2, 1]
+    So we can perform a forward num = reverse num check if we construct a reversedNum itself
+    instead of a reverse array of digits
+
+    use the digit to construct the reverse number
+    e.g. 123 -> when digit = 3
+    3 in the reverse number will be 3 * 10 ^ 2 = 300
+    2 in the reverse number will be 2 * 10 ^ 1 (+ 300) = 320 and so on...
+    the power of the digit depends on the degree of the number which would be log_10 of the number
+    e.g. 321, the highest power is in 300, with 10^2. the degree is 2.
+    */
+
+    public boolean isPalindromeSolution2(int x) {
+        if (x < 0) {return false;}
+        if (x < 10) {return true;}
+
+        int degree = (int) Math.floor(Math.log10(x));
+        long reverseNum = 0;
+
+        int i = x; //Use temp variable i for looping since we will be updating the value and we need the original number to compare in the end
+        while (i > 0) {
+            int digit = i % 10;
+
+            reverseNum += (digit * (long) Math.pow(10, degree)); //numDigits - 1 is the remaining number of digits
+            i /= 10;
+            degree--;
+        }
+        System.out.println("Reverse num: " + reverseNum);
+        return reverseNum == x;
     }
 }
